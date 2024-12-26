fbeffaa99aba66baffcff6db4aedefcf47b95eb9
Firdavsstudio555 <saidmurodov.firdavs05112009@gmail.com>
2024-12-26 22:53:43 +0500

    filter

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -1,56 +1,48 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import { Box } from "./box";
 import data from "../constant/data";
 
 export const Top = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Most popular");
+
+  // Filter the data based on the selected category
+  const filteredData = data.filter((item) =>
+    selectedCategory === "Most popular" ? true : item.category === selectedCategory
+  );
+
   return (
     <>
       <div className="w-full flex flex-col">
+        {/* Categories */}
         <div className="flex w-full justify-evenly max-w-[1092px] mt-[30px] mx-auto mb-[40px]">
-          <div className="flex items-center hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] cursor-pointer">
-            <i className="fa-solid fa-fire"></i>
-            <p>Most popular</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-regular fa-heart"></i>
-            <p>Favorites</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-bread-slice"></i>
-            <p>Bread</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-cookie"></i>
-            <p>Cookie</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-bread-slice"></i>
-            <p>Croissant</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-cake-candles"></i>
-            <p>Cupcake</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-pizza-slice"></i>
-            <p>Custard pie</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-certificate"></i>
-            <p>Donut</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-coffee"></i>
-            <p>Coffe</p>
-          </div>
-          <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
-            <i className="fa-solid fa-cake-candles"></i>
-            <p>Cake</p>
-          </div>
+          {[
+            { label: "Most popular", icon: "fa-fire" },
+            { label: "Favorites", icon: "fa-heart" },
+            { label: "Bread", icon: "fa-bread-slice" },
+            { label: "Cookie", icon: "fa-cookie" },
+            { label: "Croissant", icon: "fa-bread-slice" },
+            { label: "Cupcake", icon: "fa-cake-candles" },
+            { label: "Custard pie", icon: "fa-pizza-slice" },
+            { label: "Donut", icon: "fa-certificate" },
+            { label: "Coffe", icon: "fa-coffee" },
+            { label: "Cake", icon: "fa-cake-candles" },
+          ].map((category, index) => (
+            <div
+              key={index}
+              className={`cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 ${
+                selectedCategory === category.label ? "border-[#996531]" : "border-transparent"
+              } hover:text-[#996531] flex items-center text-[#1C140C]`}
+              onClick={() => setSelectedCategory(category.label)}
+            >
+              <i className={`fa-solid ${category.icon} mr-2`}></i>
+              <p>{category.label}</p>
+            </div>
+          ))}
         </div>
+
+        {/* Filtered Boxes */}
         <div className="w-full gap-[20px] flex flex-wrap justify-start max-w-[1100px] mx-auto overflow-y-scroll max-h-[78.2vh]">
-          {data.map((item, index) => (
+          {filteredData.map((item, index) => (
             <Box key={index} item={item} />
           ))}
         </div>
